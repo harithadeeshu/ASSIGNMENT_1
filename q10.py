0c28e718f842520f5f0695f9405889f30854621e
harithadeeshu <131469436+harithadeeshu@users.noreply.github.com>
2023-06-26 16:40:55 +0530

    Add files via upload

---
+++ q10.py
@@ -0,0 +1,10 @@
+# Q10. Write a program to find the smallest number using a stack.
+def min_value(stack):
+    x=sorted(stack)
+    return x[0]
+    
+stack=[50,7,4,3,-1]    
+min_value(stack) 
+
+stack1 = [45,67,23,55,14,88,90]
+min(*stack1)
