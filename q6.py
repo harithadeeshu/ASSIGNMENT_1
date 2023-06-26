6dfbf0ed0e5ce997ab197dd46ac66fe214afe271
harithadeeshu <131469436+harithadeeshu@users.noreply.github.com>
2023-06-26 16:33:04 +0530

    Add files via upload

---
+++ q6.py
@@ -0,0 +1,32 @@
+# Q6. Read about infix, prefix, and postfix expressions. Write a program to convert postfix to prefix expression.
+def is_operator(n):
+    if n == "+":
+        return True
+    if n == "-":
+        return True
+    if n == "/":
+        return True
+    if n == "*":
+        return True
+    return False
+
+def post_pre(x):
+    s = []
+    length = len(x)
+    for i in range(length):
+        if (is_operator(x[i])):
+            op1 = s[-1]
+            s.pop()
+            op2 = s[-1]
+            s.pop()
+            temp = x[i] + op2 + op1
+            s.append(temp)
+        else:
+             s.append(x[i])
+    ans = ""
+    for i in s:
+        ans += i
+    return ans
+
+x = "AB+CD-"
+print("Prefix : ", post_pre(x))
